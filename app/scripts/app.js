ec0c3b5ac8c1cdd956936f5ab0828bf00acfe65e
Piotr Fryga <piotr.fryga@allegro.pl>
2014-06-11 14:14:32 +0200

    yeoman angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularGenApp
+ * @description
+ * # angularGenApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularGenApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
