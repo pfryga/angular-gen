7dff04a0ac1fafeccca9972334171f53a5daf022
Piotr Fryga <piotr.fryga@allegro.pl>
2014-06-15 22:51:15 +0200

    oAuth

--- app/scripts/services/tweet.js
+++ app/scripts/services/tweet.js
@@ -8,7 +8,24 @@
  * Factory in the angularGenApp.
  */
 angular.module('angularGenApp')
-    .factory('Tweet', function ($resource) {
+    .factory('Tweet', function ($resource, $http) {
+        var consumerKey = encodeURIComponent('placeholder - key'),
+            consumerSecret = encodeURIComponent('placeholder - secret'),
+            credentials = consumerKey + ':' + consumerSecret,
+            twitterOauthEndpoint = $http.post(
+                'https://api.twitter.com/oauth2/token',
+                'grant_type=client_credentials',
+                {
+                    headers: {
+                        'Authorization': 'Basic ' + credentials,
+                        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                    }
+                }
+            );
+        twitterOauthEndpoint.success(function (response) {
+            angular.module('angularGenApp').$httpProvider.defaults.headers.common['Authorization'] = 'Bearer ' + response.access_token;
+        }).error(function (response) {});
+
         return $resource(
             'https://api.twitter.com/1.1/search/tweets.json',
             {
